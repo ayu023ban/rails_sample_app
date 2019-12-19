b54a81611ee62e68d3eb6f18b18d2e89e7e31f66
ayush <ayu023ban@gmail.com>
2019-12-19 09:40:28 +0530

    fixed some errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
-  root 'static_pages_controller#home'
+  get '/root', to:'static_pages_controller#home'
   get  '/help',    to: 'static_pages_controller#help'
   get  '/about',   to: 'static_pages_controller#about'
   get  '/contact', to: 'static_pages_controller#contact'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-  assert_template 'static_pages/home'
+  assert_template 'static_pages_controller/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
